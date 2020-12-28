d3d64803046a4c1ae5352545cb15df75a2eb9f29
SaulMontoya <saulmontoya@gidahatari.com>
2020-12-28 11:41:30 -0500

    Hakuchik try

---
+++ scripts/auxFiles/p3_flopyModel_4Oct.py
@@ -0,0 +1,201 @@
+import flopy
+import flopy.utils.binaryfile as bf
+import json, rasterio
+import geopandas as gpd
+import numpy as np
+from tqdm import tqdm
+
+#open input files
+jsonFile = open('../txt/disvDict.json', 'r')
+disvDict = json.load(jsonFile)
+
+demRaster = rasterio.open('../rst/ASTGTM2_18S_xyz2.asc')
+riversDf =  gpd.read_file('../shps/rios.shp')
+meshDf = gpd.read_file('../shps/voronoiGrid.shp')
+
+### MODFLOW Model ###
+name = 'Model'
+workspace = '../model'
+mf_exe_name = '../exe/mf6'
+
+sim = flopy.mf6.MFSimulation(sim_name=name, version='mf6',
+                             exe_name=mf_exe_name,
+                             sim_ws=workspace)
+
+tdis = flopy.mf6.ModflowTdis(sim, time_units='DAYS',
+                             perioddata=[[1.0, 1, 1.]])
+
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, save_flows=True)
+
+ims = flopy.mf6.ModflowIms(sim, print_option='SUMMARY', complexity='complex',
+                           outer_hclose=1.e-2, inner_hclose=1.e-2)
+
+#data from dictionary
+cell2d = disvDict['cell2dArrays']
+vertices = disvDict['indexedVerticesList']
+ncpl = disvDict['NCPL']
+nvert = disvDict['NVERT']
+gridIndexList = disvDict['gridIndexList']
+#another disv data
+thickRatio = [0.05,0.1,0.2,0.4,0.7,1]
+nlay = 6
+bot = 3200
+
+top = []
+
+layBotmList = []
+for cell in cell2d:
+    cellBotmList = []
+    cellCentroid = cell[1:3]
+    valuesXY = demRaster.sample([cellCentroid])
+    topCell = float(list(valuesXY)[0][0])
+    top.append(topCell)
+    for lay in range(nlay):
+        botm = bot + (topCell - bot)*(1-thickRatio[lay])
+        cellBotmList.append(botm)
+    layBotmList.append(cellBotmList)
+
+topArray = np.array(top)
+botm = np.array(layBotmList).T
+
+dis = flopy.mf6.ModflowGwfdisv(gwf, nlay=nlay, ncpl=ncpl, nvert=nvert,
+                               top=topArray, botm=botm,
+                               vertices=vertices, cell2d=cell2d)
+
+# Flujo de propiedad
+kh = [50, 50, 50, 50, 50, 50]
+icelltype = [1, 1, 0, 0, 0, 0]
+npf = flopy.mf6.ModflowGwfnpf(gwf,
+                              save_specific_discharge=True,
+                              icelltype=icelltype,
+                              k=kh,
+                              k33=kh)
+
+ic = flopy.mf6.ModflowGwfic(gwf, strt=topArray.max())
+
+rch = flopy.mf6.ModflowGwfrcha(gwf, recharge=0.001)
+
+evt = flopy.mf6.ModflowGwfevta(gwf, surface=top, rate=0.003)
+
+
+riverSpd = []
+
+# Get grid index
+intervalNumber = disvDict['intervalNumber']
+gridXarray = disvDict['gridXarray']
+gridYarray = disvDict['gridYarray']
+
+#defining function
+def findIndex(var, coordArray):
+    for interval in range(intervalNumber):
+#        if var > coordArray[interval] and var < coordArray[interval+1]:
+        if var > coordArray[interval] and var < coordArray[interval+1]:
+            return interval
+            break
+
+i = 0
+
+print('\nWorking with the spatial index of rivers')
+riverCellsList = []
+riverSpd = []
+
+riversDf[["xInterBeg","xInterEnd","yInterBeg","yInterEnd"]] = 0
+for riverIndex, riverRow in tqdm(riversDf.iterrows(), total= riversDf.shape[0]):
+    coords = riverRow.geometry.bounds
+    xmin = coords[0]
+    xmax = coords[2]
+    ymin = coords[1]
+    ymax = coords[3]
+    xInterBeg = findIndex(xmin,gridXarray)
+    xInterEnd = findIndex(xmax,gridXarray)
+    yInterBeg = findIndex(ymin,gridYarray)
+    yInterEnd = findIndex(ymax,gridYarray)
+    riversDf.loc[riverIndex,"xInterBeg"] = xInterBeg
+    riversDf.loc[riverIndex,"xInterEnd"] = xInterEnd
+    riversDf.loc[riverIndex,"yInterBeg"] = yInterBeg
+    riversDf.loc[riverIndex,"yInterEnd"] = yInterEnd
+print(riversDf.head())
+print(riversDf.describe())
+
+
+for xInt in range(intervalNumber):
+    for yInt in range(intervalNumber):
+        #filter river cells in the gridrectangle
+        filterDf = riversDf[(xInt >= riversDf["xInterBeg"]) & ((xInt+1) <= riversDf["xInterEnd"])
+                            & ((yInt) <= riversDf["yInterBeg"]) & ((yInt+1) <= riversDf["yInterEnd"])]# & (riversDf["yInterEnd"]<=yInt+1)]
+        if filterDf.count()[0]>0:
+            print(xInt,yInt)
+            print(filterDf["xInterEnd"].count()[0])
+        #filterDf = filterDf[(filterDf["xInterEnd"]<=(xInt+1)) & (filterDf["yInterEnd"]<=(yInt+1))]
+        #print(filterDf.describe())
+        #if filterDf.count()[0]>0:
+        #    print(filterDf.describe())
+        #    print(xInt,yInt)
+
+        #get river in this interval
+
+print('\nWorking with the spatial index of rivers')
+riverCellsList = []
+riverSpd = []
+for riverIndex, riverRow in tqdm(riversDf.iterrows(), total= riversDf.shape[0]):
+    coords = riverRow.geometry.bounds
+    xmin = coords[0]
+    xmax = coords[2]
+    ymin = coords[1]
+    ymax = coords[3]
+    xInterBeg = findIndex(xmin,gridXarray)
+    xInterEnd = findIndex(xmax,gridXarray)
+    yInterBeg = findIndex(ymin,gridYarray)
+    yInterEnd = findIndex(ymax,gridYarray)
+    #xinterCells
+    if xInterBeg == xInterEnd:
+        xInterCells = [xInterBeg]
+    elif xInterBeg < xInterEnd:
+        xInterCells = list(range(xInterBeg,xInterEnd+1))
+    else: print('\nSomething is going on with the xInterCells')
+    #yinterCells
+    if yInterBeg == yInterEnd:
+        yInterCells = [yInterBeg]
+    elif yInterBeg < yInterEnd:
+        yInterCells = list(range(yInterBeg,yInterEnd+1))
+    else: print('\nSomething is going on with the xInterCells')
+
+    for cellIndex, cellRow in enumerate(gridIndexList):
+        if cellIndex not in riverCellsList:
+            for xCell in xInterCells:
+                for yCell in xInterCells:
+                    #Get cells that are in the same grid index
+                    if xCell in cellRow[0] and yCell in cellRow[1]:
+                        cellPolyRow = meshDf.iloc[cellIndex]
+                        if riverRow.geometry.crosses(cellPolyRow.geometry):
+                            #riverSpd.append([(0,index),top[index],1e5,top[index]-2])
+                            riverSpd.append([(0,cellIndex),top[cellIndex],1.5])
+                            riverCellsList.append(cellIndex)
+
+
+#for polyIndex,polyRow in tqdm(meshDf.iterrows(), total= meshDf.shape[0]):
+#    for riverIndex, riverRow in riversDf.iterrows():
+#        if riverRow.geometry.crosses(polyRow.geometry):
+#            riverSpd.append([(0,polyIndex),top[polyIndex],1e5,top[polyIndex]-2])
+riv_spd = {0: riverSpd}
+#riv = flopy.mf6.ModflowGwfriv(gwf, stress_period_data=riv_spd)
+riv = flopy.mf6.ModflowGwfdrn(gwf, stress_period_data=riv_spd)
+
+
+# wel
+#wel_spd = {0: [[(2, 183), -45000],
+#                [(2, 18), -45000]
+#                ]}
+#wel = flopy.mf6.ModflowGwfwel(gwf,stress_period_data=wel_spd)
+
+# oc
+hname = '{}.hds'.format(name)
+cname = '{}.cbc'.format(name)
+oc = flopy.mf6.ModflowGwfoc(gwf, budget_filerecord=cname,
+                            head_filerecord=hname,
+                            saverecord=[('HEAD', 'ALL'), ('BUDGET', 'ALL')])
+
+# Escribir archivos y correr simulación
+print('\nWriting model final and model simulation')
+sim.write_simulation()
+sim.run_simulation()
---
+++ scripts/auxFiles/scripts_10Oct/p2_vertices2d.py
@@ -0,0 +1,93 @@
+import os, json
+import numpy as np
+import geopandas as gpd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+# Open vector and raster files
+vorMesh = gpd.read_file('../shps/voronoiGrid.shp')
+
+# Get grid index
+intervalNumber = 10
+meshBounds = vorMesh.total_bounds
+gridXarray = np.linspace(meshBounds[0],meshBounds[2],intervalNumber+1)
+gridYarray = np.linspace(meshBounds[1],meshBounds[3],intervalNumber+1)
+
+totalVerticesList = []
+cell2dArrays = []
+polygonCentroidList = []
+gridIndexList =[]
+
+#defining function
+def findIndex(var, coordArray):
+    for interval in range(intervalNumber):
+#        if var > coordArray[interval] and var < coordArray[interval+1]:
+        if var > coordArray[interval] and var < coordArray[interval+1]:
+            return interval
+            break
+
+print('\nCreating a unique list of vertices [[x1,y1],[x2,y2],...]')
+for index,row in tqdm(vorMesh.iterrows(), total= vorMesh.shape[0]):
+    #vertices xy
+    coords = row.geometry.exterior.coords.xy
+    totalVerticesList += list(zip(coords[0],coords[1]))
+
+uniqueVerticesArray = np.unique(np.array(totalVerticesList),axis=0)
+uniqueVerticesList = uniqueVerticesArray.tolist()
+
+vertexIndexDict = {}
+for index, vertex in enumerate(uniqueVerticesList):
+    srtVertex = str(vertex)
+    vertexIndexDict[srtVertex]=index
+
+print('\nExtracting cell2d data and grid index')
+for index,row in tqdm(vorMesh.iterrows(), total= vorMesh.shape[0]):
+    rowGeometry = row.geometry
+    coords = rowGeometry.exterior.coords
+    #cell2d array
+    cellArray = []
+    #add index
+    cellArray.append(index)
+    #add centroid
+    cellArray += list(rowGeometry.centroid.coords[0])
+    #working with vertices number and vertex
+    vertexIndexList = []
+    for vertex in coords:
+        srtVertex = str(list(vertex))
+        vertexIndexList.append(vertexIndexDict[srtVertex])
+    cellArray.append(len(vertexIndexList))
+    cellArray += vertexIndexList
+    cell2dArrays.append(cellArray)
+    #get grid index
+    xmin = min(coords.xy[0])
+    xmax = max(coords.xy[0])
+    ymin = min(coords.xy[1])
+    ymax = max(coords.xy[1])
+    xInterBeg = findIndex(xmin,gridXarray)
+    xInterEnd = findIndex(xmax,gridXarray)
+    yInterBeg = findIndex(ymin,gridYarray)
+    yInterEnd = findIndex(ymax,gridYarray)
+    gridIndexList.append([[xInterBeg,xInterEnd],[yInterBeg,yInterEnd]])
+
+uniqueVerticesArray = np.unique(np.array(totalVerticesList),axis=0)
+uniqueVerticesList = uniqueVerticesArray.tolist()
+indexedVerticesList = [[index, row[0], row[1]] for index, row in enumerate(uniqueVerticesList)]
+
+disvDict = {}
+disvDict['NCPL'] = len(vorMesh.index)
+disvDict['NVERT'] = len(uniqueVerticesList)
+disvDict['uniqueVerticesList']=uniqueVerticesList
+disvDict['indexedVerticesList']=indexedVerticesList
+disvDict['cell2dArrays'] = cell2dArrays
+
+spatialIndexDict = {}
+spatialIndexDict['intervalNumber'] = intervalNumber
+spatialIndexDict['gridXarray'] = list(gridXarray)
+spatialIndexDict['gridYarray'] = list(gridYarray)
+spatialIndexDict['gridIndexList'] = gridIndexList
+
+with open('../txt/disvDict.json', 'w') as outf:
+    json.dump(disvDict, outf)
+
+with open('../txt/spatialIndexDict.json', 'w') as outf:
+    json.dump(spatialIndexDict, outf)
